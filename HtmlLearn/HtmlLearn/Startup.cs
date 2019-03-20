530b08b63ca250f603956ac79b2dd742f07ee869
龙俊升 <1403300189@qq.com>
2019-03-20 22:30:50 +0800

    添加求助页面

--- HtmlLearn/HtmlLearn/Startup.cs
+++ HtmlLearn/HtmlLearn/Startup.cs
@@ -31,7 +31,9 @@ namespace HtmlLearn
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                //.SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
