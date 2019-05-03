da9764b11a779db083ab240bf6a8408febca5178
俊升 <1403300189@qq.com>
2019-05-03 22:54:11 +0800

    正则表达式

--- HtmlLearn/17bang/wwwroot/js/事件.js
+++ HtmlLearn/17bang/wwwroot/js/事件.js
@@ -1,5 +1,5 @@
 ﻿////1.参考求助首页侧边栏关键字，实现：当鼠标移动到关键字上，显示关键字被使用次数
-//var btns = document.getElementById("keyword").getElementsByTagName("a")
+//let btns = document.getElementById("keyword").getElementsByTagName("a");
 //for (var i = 0; i < btns.length; i++) {
 //    btns[i].onmouseover = function () {
 //        console.log("该关键字已被使用" + this.getAttribute('data-used') + "次");
@@ -9,10 +9,10 @@
 ////2.参考文章的全系列阅读功能，当滚动到页面底部时，将页面现有内容粘贴到页面底部，直到5次过后，在页面底部显示：已经没有更多内容了
 ////3.参考消息页面：完成勾选全选功能
 
-//var a = document.getElementsByName("filter")[0].getElementsByTagName("input")  //所有的框
-//    a[2].onclick = function () {  //全选框
+//let input = document.getElementsByName("filter")[0].getElementsByTagName("input");  //所有的框
+//    input[2].onclick = function () {  //全选框
 //        for (var i = 0; i < a.length; i++) {
-//            a[i].checked = a[2].checked
+//            input[i].checked = input[2].checked;
 //        }
 //    }
 
@@ -21,10 +21,10 @@
 ////(2).重新输入用户名，一旦输入用户名部分没有重复，“用户名重复”的提示消失
 ////(3).当用户名或密码等有值时，关闭页面前弹出确认框
 
-//var arr = ["laocheng", "laozhao", "wenxuan"];
-//var User = document.getElementsByTagName("input")[2];
+//let arr = ["laocheng", "laozhao", "wenxuan"];
+//let User = document.getElementsByTagName("input")[2];
 //document.getElementsByTagName("input")[6].onsubmit = function (event) {
-//    for (var i = 0; i < arr.length; i++) {
+//    for (let i = 0; i < arr.length; i++) {
 //        if (this.value === arr[i]) {
 //            event.preventDefault(); //阻止form提交
 //            document.getElementsByTagName("h4")[0].style.display = "block";
@@ -53,24 +53,24 @@
 ////5.参考求助首页侧边栏“免费广告发布”弹出Modal，实现（不需要弹出Modal）：
 ////(1.)根据下拉列表选中项，填充“文字内容”和“链接”文本框
 ////(2.)勾选修改的checkbox，实现“文字内容”和“链接”文本框的禁用和启用
-//var list = document.getElementsByTagName("select")[0]
-//var ad = document.getElementsByTagName("input")
+//let list = document.getElementsByTagName("select")[0];
+//let input = document.getElementsByTagName("input");
 //list.onclick = function () {
 //    if (list.value === "1") {
-//        ad[7].value = "点击赠送屠龙宝刀 一刀999级不是梦 是兄弟就来砍我。"
-//        ad[8].value = "https://user.qzone.qq.com/1403300189/infocenter"
-//        ad[7].readOnly = " readonly"
-//        ad[8].readOnly = " readonly"
+//        input[7].value = "点击赠送屠龙宝刀 一刀999级不是梦 是兄弟就来砍我。";
+//        input[8].value = "https://user.qzone.qq.com/1403300189/infocenter";
+//        input[7].readOnly = " readonly";
+//        input[8].readOnly = " readonly";
 //    } else if (list.value === "2") {
-//        ad[7].value = "c#天下第一，JAVA就是弟弟"
-//        ad[8].value = "https://http://17bang.ren/Problem"
-//        ad[7].readOnly = "readonly"
-//        ad[8].readOnly = "readonly"
+//        input[7].value = "c#天下第一，JAVA就是弟弟";
+//        input[8].value = "https://http://17bang.ren/Problem";
+//        input[7].readOnly = "readonly";
+//        input[8].readOnly = "readonly";
 //    } else {
-//        ad[7].value = ""
-//        ad[8].value = ""
-//        ad[7].readOnly = ""
-//        ad[8].readOnly = ""
+//        input[7].value = "";
+//        input[8].value = "";
+//        input[7].readOnly = "";
+//        input[8].readOnly = "";
 //    }
 //}
 
@@ -80,118 +80,102 @@
 ////(3.)一级关键字和二级关键字被选中后，会显示在下拉列表上方
 ////(4.)自定义关键字输入完成后（按空格键），输入的关键字显示在下拉列表上方
 ////(5.)后输入的关键字显示在前面
-var DownMenu = document.getElementsByClassName("input-group")[0].getElementsByClassName("dropdown-toggle")
-//一级关键字下拉栏
-var Spread = document.getElementsByClassName("input-group-btn")
-//二级关键字下拉栏
-var oneKeyWord = document.getElementsByClassName("dropdown-menu")[0].getElementsByTagName("a")
-//一级关键字
-var twoKeyWord = document.getElementsByClassName("input-group-btn")[1].getElementsByTagName("a")
-//二级关键字
-DownMenu[0].onclick = function () {
-    if (Spread[0].className === "input-group-btn") {
-        Spread[0].classList = "input-group-btn open"
-    } else {
-        Spread[0].classList = "input-group-btn"
-    }
-}//一级关键字下拉栏显示
-DownMenu[1].onclick = function () {
-    if (Spread[1].className === "input-group-btn") {
-        Spread[1].classList = "input-group-btn open"
-    } else {
-        Spread[1].classList = "input-group-btn"
-    }
-}//二级关键字下拉栏显示
+//var DownMenu = document.getElementsByClassName("input-group")[0].getElementsByClassName("dropdown-toggle");
+////一级关键字下拉栏
+//var Spread = document.getElementsByClassName("input-group-btn");
+////二级关键字下拉栏
+//var oneKeyWord = document.getElementsByClassName("dropdown-menu")[0].getElementsByTagName("a");
+////一级关键字
+//var twoKeyWord = document.getElementsByClassName("input-group-btn")[1].getElementsByTagName("a");
+////二级关键字
+//DownMenu[0].onclick = function () {
+//    if (Spread[0].className === "input-group-btn") {
+//        Spread[0].classList = "input-group-btn open";
+//    } else {
+//        Spread[0].classList = "input-group-btn";
+//    }
+//}//一级关键字下拉栏显示
+//DownMenu[1].onclick = function () {
+//    if (Spread[1].className === "input-group-btn") {
+//        Spread[1].classList = "input-group-btn open";
+//    } else {
+//        Spread[1].classList = "input-group-btn";
+//    }
+//}//二级关键字下拉栏显示
 
-for (let i = 0; i < oneKeyWord.length; i++) {
-    oneKeyWord[i].onclick = function (event) {
-        //一级关键字点击事件
-        event.preventDefault()
-        //分割
-        let div = document.createElement("div")
-        div.setAttribute("class", "badge")
-        div.style.marginRight = "15px"
-        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.append(div)
-        let span = document.createElement("span")
-        div.appendChild(span)
-        let text = document.createTextNode(this.outerText)
-        span.appendChild(text)
-        let badge = document.createElement("span")
-        badge.setAttribute("class", "glyphicon glyphicon-remove-sign")
-        span.appendChild(badge)
-        //点击在下拉列表上方显示
-        for (let i = 0; i < oneKeyWord.length; i++) {
-            document.querySelectorAll("ul.dropdown-menu")[i + 1].style.display = "none"
-        }
-        let k = +this.getAttribute('data-used')
-        document.querySelectorAll("ul.dropdown-menu")[k].removeAttribute("style")
-    }   //判断点击事件显示二级关键字
-}
+//for (let i = 0; i < oneKeyWord.length; i++) {
+//    oneKeyWord[i].onclick = function (event) {
+//        //一级关键字点击事件
+//        event.preventDefault();
+//        //分割
+//        let div = document.createElement("div")
+//        div.setAttribute("class", "badge");
+//        div.style.marginRight = "15px";
+//        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.append(div);
+//        let span = document.createElement("span");
+//        div.appendChild(span);
+//        var text = document.createTextNode(this.childNodes[0].wholeText);
+//        span.appendChild(text);
+//        let badge = document.createElement("span");
+//        badge.setAttribute("class", "glyphicon glyphicon-remove-sign");
+//        span.appendChild(badge);
+//        //点击在下拉列表上方显示
+//        for (let i = 0; i < oneKeyWord.length; i++) {
+//            document.querySelectorAll("ul.dropdown-menu")[i + 1].style.display = "none";
+//        }
+//        let k = +this.getAttribute('data-used');
+//        document.querySelectorAll("ul.dropdown-menu")[k].removeAttribute("style");
+//    }   //判断点击事件显示二级关键字
+//}
 
-for (let i = 0; i < twoKeyWord.length; i++) {
-    twoKeyWord[i].onclick = function (event) {
-        event.preventDefault()
-        //二级关键字点击在下拉列表上方显示
-        let div = document.createElement("div")
-        div.setAttribute("class", "badge")
-        div.style.marginRight = "15px"
-        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.append(div)
-        let span = document.createElement("span")
-        div.appendChild(span)
-        let text = document.createTextNode(this.outerText)
-        span.appendChild(text)
-        let badge = document.createElement("span")
-        badge.setAttribute("class", "glyphicon glyphicon-remove-sign")
-        span.appendChild(badge)
-    }
-}
-
-document.getElementsByClassName("form-control")[2].onkeydown = function (event) {
-    if (event.keyCode == 32 || event.keyCode == 13) {
-        //判断键盘按下的键32是空格，13是回车
-        let div = document.createElement("div")
-        div.setAttribute("class", "badge")
-        div.style.marginRight = "15px"
-        let first = document.getElementsByClassName("gap")[3].getElementsByTagName("div")[0].firstElementChild
-        //获取第一个元素
-        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.insertBefore(div,first)
-        //把添加的元素始终丢前面
-        let span = document.createElement("span")
-        div.appendChild(span)
-        let text = document.createTextNode(this.value)
-        span.appendChild(text)
-        let badge = document.createElement("span")
-        badge.setAttribute("class", "glyphicon glyphicon-remove-sign")
-        span.appendChild(badge)
-    }
-}
-
-
-
-
-
-
-//for (var i = 0; i < KeyWord.length; i++) {
-//    KeyWord[i].onclick = function (event) {
-//        event.preventDefault()
-//        var j = this.getAttribute('data-used')
-//        document.getElementsByName(j)[0].style.display = "inline-block"
+//for (let i = 0; i < twoKeyWord.length; i++) {
+//    twoKeyWord[i].onclick = function (event) {
+//        event.preventDefault();
+//        //二级关键字点击在下拉列表上方显示
+//        let div = document.createElement("div");
+//        div.setAttribute("class", "badge");
+//        div.style.marginRight = "15px";
+//        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.append(div);
+//        let span = document.createElement("span");
+//        div.appendChild(span);
+//        let text = document.createTextNode(this.childNodes[0].wholeText);
+//        span.appendChild(text);
+//        let badge = document.createElement("span");
+//        badge.setAttribute("class", "glyphicon glyphicon-remove-sign");
+//        span.appendChild(badge);
 //    }
 //}
-//for (var i = 0; i < KeyWord1.length; i++) {
-//    KeyWord1[i].onclick = function (event) {
-//        event.preventDefault()
-//        var k = this.getAttribute('data-used')
-//        document.getElementsByName(k)[0].style.display = "inline-block"
+
+//document.getElementsByClassName("form-control")[2].onkeydown = function (event) {
+//    if (event.keyCode == 32 || event.keyCode == 13) {
+//        //判断键盘按下的键32是空格，13是回车
+//        let div = document.createElement("div");
+//        div.setAttribute("class", "badge");
+//        div.style.marginRight = "15px";
+//        let first = document.getElementsByClassName("gap")[3].getElementsByTagName("div")[0].firstElementChild;
+//        //获取第一个元素
+//        document.getElementsByClassName("gap")[3].getElementsByTagName("label")[0].nextElementSibling.insertBefore(div, first);
+//        //把添加的元素始终丢前面
+//        let span = document.createElement("span");
+//        div.appendChild(span);
+//        let text = document.createTextNode(this.value);
+//        span.appendChild(text);
+//        let badge = document.createElement("span");
+//        badge.setAttribute("class", "glyphicon glyphicon-remove-sign");
+//        span.appendChild(badge);
 //    }
 //}
 
 
+//document.body.onscroll = function () {
+//    var height = document.body.clientHeight
+//    var ContentHeight = document.body.offsetHeight
+//    var scroll = document.body.scrollTop
+//    var body = document.body.innerHTML
+//    if (scroll === (ContentHeight - height)) {
+//        document.getElementsByTagName("main")[0].nextElementSibling.append(body);
+//    }
+//}
+////滚动条下拉！！！！！！！！
 
-
-
-
-
-
-//二级关键字展开 的ul表怎么搞
 
---
+++ HtmlLearn/17bang/wwwroot/js/正则表达式.js
@@ -0,0 +1,42 @@
+﻿//1.完成详细的、可测试的需求文档：学费计算器（Web版）。实现以下功能：
+//(1.)用户可以选择在源栈学习的开始时间、共学习多少天 / 周 / 月
+//(2.)然后，系统自动计算出截至日期和总学费
+
+
+
+
+//2.使用正则表达式判断某个字符串:
+//  （1.）是否是合格的Email格式
+//  （2.）是否是小数（正负小数都可以）
+//  （3.）将所有以zyf - 开头的属性去掉zyf - （尽可能多的制造测试用例，比如：<a lzyf-old=''， 或者：<span>zyf---+---fyz</span> ……）
+
+//var pattern = /(?!\s+)[A-Za-z0-9]+@.+\.([A-Za-z]+)/,
+//    str = '1403300189@qq.com';
+//console.log(pattern.test(str));
+
+
+//var pattern = /(^[0-9]*)\.*([0-9]*$)/,
+//    str = "156148651456.156156"
+//console.log(pattern.test(str))
+
+//^zyf\-*
+
+
+//3.JSON生成和解析：
+//(1.)按自己的情况，生成一个JSON字符串，包括真实姓名、QQ昵称、年龄、性别、兴趣爱好、自我介绍……，上传到QQ群：一起帮·有意向（729600626）
+//(2.)根据其他同学的JSON获得其个人资料，生成一个表格显示。
+
+
+//var XiaoCheng = {
+//    name: "小程",
+//    QQname: "老程",
+//    age: 41,
+//    gender: "女",
+//    hobby: "blackmail,cheat,thief",
+//    introduction:"Never Mind the Scandal and Liber"
+//}
+//console.log(JSON.stringify(XiaoCheng, function (key, value) {
+//    return value
+//}))
+
+var lw = JSON.parse('{ "name": "小程", "QQname": "老程", "age": 41, "gender": "女", "hobby": "blackmail,cheat,thief", "introduction": "Never Mind the Scandal and Liber" }')
